82808342a81a00a1a1fb3ff0998434e63d8859d4
Kenchu123 <k88913n@gmail.com>
2017-04-06 17:38:16 +0800

    simple-login

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Button, Form, Container, Menu } from 'semantic-ui-react'
 
-const App = () => (
+const Login_Form = () => (
   <div>
-    <h1>
-      Jizz
-    </h1>
+    <Menu secondary inverted style={{background: 'black', margin: '0'}}>
+        <Menu.Item name="jizz1" />
+        <Menu.Item name="Jizz2" />
+      </Menu>
+    <Container>
+      <h1>INFOR-PAD LOGIN</h1>
+      <Form>
+        <Form.Field>
+          <label>Username:</label>
+          <input placeholder='Input your Username' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password:</label>
+          <input type='password' placeholder='Input your Password' />
+        </Form.Field>
+        <Button type='submit'>Login</Button>
+      </Form>
+    </Container>
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<Login_Form />, document.getElementById('app'))
