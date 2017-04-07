03d46f502635d8354386ff67cfa36cd99aac4a6a
Leo Chen <leo881003@gmail.com>
2017-04-07 12:30:36 +0800

    Add temporary sign up

---
+++ routes/api-user.js
@@ -0,0 +1,16 @@
+var router = require('koa-router')()
+const crypto = require('crypto')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
+router.post('/user', async ctx => {
+    let data = ctx.request.body
+    let hash = crypto.createHash('sha512').update(data.password).digest('hex')
+    new User({
+        name : data.username,
+        password : hash//,
+        //root : { type: Schema.Types.ObjectId, ref: 'Directory' }
+    })//TODO:only for temporary usage
+})
+
+module.exports = router
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 const session = require('./api-session')
+const user = require('./api-user')
 var router = require('koa-router')()
 
 router.prefix('/api');
 
 router.use(session.routes(), session.allowedMethods())
+router.use(user.routes(), user.allowedMethods())
 
 module.exports = router
