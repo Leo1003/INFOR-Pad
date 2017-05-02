a2f7538a01a0cbd3612b5ea7fba3bae7b56f31f3
Kenchu123 <k88913n@gmail.com>
2017-05-02 12:24:34 +0800

    https css

--- src/server/router.js
+++ src/server/router.js
@@ -47,7 +47,7 @@ function renderPage(appHtml, preloadedState) {
     src="https://code.jquery.com/jquery-3.1.1.min.js"
     integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
     crossorigin="anonymous"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.js"></script>
     </head>
     <body>
       <div id="app">${appHtml}</div>
