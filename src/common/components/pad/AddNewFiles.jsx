8e68ada329a28672c228b28551d3f798cd372681
Kenchu123 <k88913n@gmail.com>
2017-05-03 18:29:31 +0800

    form in modal

--- src/common/components/pad/AddNewFiles.jsx
+++ src/common/components/pad/AddNewFiles.jsx
@@ -4,21 +4,69 @@ import { Dropdown, Modal, Header, Button, Icon } from 'semantic-ui-react'
 class AddNewFiles extends React.Component {
   constructor(props) {
     super(props)
-    this.handleAddNewFolder = this.handleAddNewFolder.bind(this)
-    this.addNewFolder = this.addNewFolder.bind(this)
-    this.addNewFile = this.addNewFile.bind(this)
-  }
-  handleAddNewFolder(e, data) {
-    console.log("addnewfolder")
-    console.log(data)
+    this.newFolderModal = this.newFolderModal.bind(this)
+    this.newFileModal = this.newFileModal.bind(this)
   }
   componentDidMount() {
+    $('#addfileform').form({
+      fields: {
+        filename: {
+          identifier: 'filename',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter a File Name'
+            }
+          ]
+        }
+      },
+      inline: true,
+      onFailure: this.handleInvalid.bind(this),
+      onSuccess: this.handleAddNewFile.bind(this)
+    })
+    $('#addfolderform').form({
+      fields: {
+        foldername: {
+          identifier: 'foldername',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter a Folder Name'
+            }
+          ]
+        }
+      },
+      inline: true,
+      onFailure: this.handleInvalid.bind(this),
+      onSuccess: this.handleAddNewFolder.bind(this)
+    })
   }
-  addNewFolder() {
-    $('.ui.small.modal.addNewFolderModal').modal({ blurring: true }).modal('show')
+  handleAddNewFile(e) {
+    console.log("addnewfile")
+    e.preventDefault()
   }
-  addNewFile() {
-    $('.ui.small.modal.addNewFileModal').modal({ blurring: true }).modal('show')
+  handleAddNewFolder(e) {
+    console.log("addnewfolder")
+    e.preventDefault()
+  }
+  handleInvalid(e) {
+    return false
+  }
+  newFolderModal() {
+    $('.ui.small.modal.addNewFolderModal').modal({
+      blurring: true,
+      onDeny: () => {
+        $('#addfolderform').form('reset')
+      }
+      }).modal('show')
+  }
+  newFileModal() {
+    $('.ui.small.modal.addNewFileModal').modal({
+      blurring: true,
+      onDeny: () => {
+        $('#addfileform').form('reset')
+      }
+    }).modal('show')
   }
   render() {
     return (
@@ -27,8 +75,8 @@ class AddNewFiles extends React.Component {
           <i className='plus icon'></i>
           <div className="text">New</div>
           <div className="menu">
-            <div className="item" onClick={this.addNewFolder}><i className='folder icon'></i>New Folder</div>
-            <div className="item" onClick={this.addNewFile}><i className='file icon'></i>New File</div>
+            <div className="item" onClick={this.newFolderModal}><i className='folder icon'></i>New Folder</div>
+            <div className="item" onClick={this.newFileModal}><i className='file icon'></i>New File</div>
           </div>
         </div>
 
@@ -38,17 +86,23 @@ class AddNewFiles extends React.Component {
             Add a new Folder
           </div>
           <div className="content">
-            <p>Folder Form</p>
+
+            <form className="ui folder form" id="addfolderform">
+              <div className="fields">
+                <input type="text" name="foldername" ref='foldername' placeholder='Folder Name...' />
+              </div>
+            </form>
+
           </div>
           <div className="actions">
             <div className="ui basic cancel button">
               <i className="remove icon"></i>
               Cancel
             </div>
-            <div className="ui blue ok button">
+            <button className="ui blue button" type="submit" form='addfolderform'>
               <i className="checkmark icon"></i>
               Add
-            </div>
+            </button>
           </div>
         </div>
 
@@ -58,17 +112,23 @@ class AddNewFiles extends React.Component {
             Add a new File
           </div>
           <div className="content">
-            <p>File Form</p>
+
+            <form className="ui fileform form" id="addfileform">
+              <div className="field">
+                <input type="text" name="filename" ref='filename' placeholder='File Name...' />
+              </div>
+            </form>
+
           </div>
           <div className="actions">
             <div className="ui basic cancel button">
               <i className="remove icon"></i>
-              No
+              Cancel
             </div>
-            <div className="ui blue ok button">
+            <button className="ui blue button" type="submit" form='addfileform'>
               <i className="checkmark icon"></i>
-              Yes
-            </div>
+              Add
+            </button>
           </div>
         </div>
       </div>
