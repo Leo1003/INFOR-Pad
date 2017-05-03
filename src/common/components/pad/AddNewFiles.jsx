bfc4fbb1094b47822182b7d38431256e745ff574
Kenchu123 <k88913n@gmail.com>
2017-05-03 17:06:04 +0800

    add files modal

--- src/common/components/pad/AddNewFiles.jsx
+++ src/common/components/pad/AddNewFiles.jsx
@@ -5,6 +5,8 @@ class AddNewFiles extends React.Component {
   constructor(props) {
     super(props)
     this.handleAddNewFolder = this.handleAddNewFolder.bind(this)
+    this.addNewFolder = this.addNewFolder.bind(this)
+    this.addNewFile = this.addNewFile.bind(this)
   }
   handleAddNewFolder(e, data) {
     console.log("addnewfolder")
@@ -12,28 +14,65 @@ class AddNewFiles extends React.Component {
   }
   componentDidMount() {
   }
+  addNewFolder() {
+    $('.ui.small.modal.addNewFolderModal').modal({ blurring: true }).modal('show')
+  }
+  addNewFile() {
+    $('.ui.small.modal.addNewFileModal').modal({ blurring: true }).modal('show')
+  }
   render() {
     return (
-      <Dropdown button text='New' basic>
-        <Dropdown.Menu>
-          <Modal dimmer='blurring' trigger={<Dropdown.Item icon='folder' text='New Folder' />} size='small'>
-            <Header icon='folder' content='Add a new folder' />
-            <Modal.Content>
-              <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
-            </Modal.Content>
-            <Modal.Actions>
-              <Button basic color='red'>
-                <Icon name='remove' /> Cancel
-              </Button>
-              <Button basic color='green'>
-                <Icon name='checkmark' /> Build
-              </Button>
-            </Modal.Actions>
-          </Modal>
+      <div>
+        <div className="ui dropdown button basic">
+          <i className='plus icon'></i>
+          <div className="text">New</div>
+          <div className="menu">
+            <div className="item" onClick={this.addNewFolder}><i className='folder icon'></i>New Folder</div>
+            <div className="item" onClick={this.addNewFile}><i className='file icon'></i>New File</div>
+          </div>
+        </div>
 
-          <Dropdown.Item icon='file' text='New File' />
-        </Dropdown.Menu>
-      </Dropdown>
+        <div className="ui small modal addNewFolderModal">
+          <div className="ui icon header">
+            <i className="folder icon"></i>
+            Add a new Folder
+          </div>
+          <div className="content">
+            <p>Folder Form</p>
+          </div>
+          <div className="actions">
+            <div className="ui basic cancel button">
+              <i className="remove icon"></i>
+              Cancel
+            </div>
+            <div className="ui blue ok button">
+              <i className="checkmark icon"></i>
+              Add
+            </div>
+          </div>
+        </div>
+
+        <div className="ui small modal addNewFileModal">
+          <div className="ui icon header">
+            <i className="file icon"></i>
+            Add a new File
+          </div>
+          <div className="content">
+            <p>File Form</p>
+          </div>
+          <div className="actions">
+            <div className="ui basic cancel button">
+              <i className="remove icon"></i>
+              No
+            </div>
+            <div className="ui blue ok button">
+              <i className="checkmark icon"></i>
+              Yes
+            </div>
+          </div>
+        </div>
+      </div>
+
     )
   }
 }
