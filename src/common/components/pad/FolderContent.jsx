697c909e4dff41e518d4a5609d8791dfe4f61cea
Kenchu123 <k88913n@gmail.com>
2017-05-13 23:16:29 +0800

    choose to render all public private

--- src/common/components/pad/FolderContent.jsx
+++ src/common/components/pad/FolderContent.jsx
@@ -9,6 +9,12 @@ const moment = require('moment')
 class FolderContent extends React.Component {
   constructor(props) {
     super(props)
+    this.showAll = this.showAll.bind(this)
+    this.showPublic = this.showPublic.bind(this)
+    this.showPrivate = this.showPrivate.bind(this)
+    this.state = {
+      show: 'All'
+    }
   }
   componentDidMount() {
     $('.ui.dropdown').dropdown()
@@ -16,8 +22,22 @@ class FolderContent extends React.Component {
   componentWillReceiveProps() {
     $('.ui.dropdown').dropdown()
   }
+  showAll() {
+    this.setState({
+      show: 'All'
+    })
+  }
+  showPublic() {
+    this.setState({
+      show: 'Public'
+    })
+  }
+  showPrivate() {
+    this.setState({
+      show: 'Private'
+    })
+  }
   render() {
-
     return (
       <div>
         <h2>{this.props.folder.name}</h2>
@@ -26,7 +46,9 @@ class FolderContent extends React.Component {
           <thead>
             <tr>
               <th colSpan="4">
-                Data:
+                  <button className="tiny ui basic button" onClick={this.showAll}>All</button>
+                  <button className="tiny ui basic button" onClick={this.showPublic}>Public</button>
+                  <button className="tiny ui basic button" onClick={this.showPrivate}>Private</button>
               </th>
             </tr>
           </thead>
@@ -39,7 +61,7 @@ class FolderContent extends React.Component {
               </tr>
             ) : null}
             {this.props.folder.files.map(file => {
-              if(file.format === 'Directory')
+              if(file.format === 'Directory' && (this.state.show === 'All' || (this.state.show === 'Public' && file.isPublic) || (this.state.show === 'Private' && !file.isPublic)))
                 return (
                   <tr key={file.id}>
                     <td className='collapsing'>
@@ -57,7 +79,7 @@ class FolderContent extends React.Component {
               })}
 
             {this.props.folder.files.map(file => {
-              if(file.format !== 'Directory')
+              if(file.format !== 'Directory' && (this.state.show === 'All' || (this.state.show === 'Public' && file.isPublic) || (this.state.show === 'Private' && !file.isPublic)))
                 return (
                 <tr key={file.id}>
                   <td className='collapsing'>
