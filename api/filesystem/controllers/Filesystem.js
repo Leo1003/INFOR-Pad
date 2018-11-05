44b9778689e6a1b90c822d682c78e49747614a5e
Leo <leo881003@gmail.com>
2018-11-06 01:46:06 +0800

    Starting migrating

--- api/filesystem/controllers/Filesystem.js
+++ api/filesystem/controllers/Filesystem.js
@@ -1,68 +1,101 @@
 'use strict';
 
 /**
- * Filesystem.js controller
- *
- * @description: A set of functions called "actions" for managing `Filesystem`.
- */
+* Filesystem.js controller
+*
+* @description: A set of functions called "actions" for managing `Filesystem`.
+*/
 
 module.exports = {
 
-  /**
-   * Retrieve filesystem records by short id
-   *
-   * @return {Object|Array}
-   */
+    /**
+    * Retrieve files / directories by short id
+    *
+    * @return {Object|Array}
+    */
 
-  get: async (ctx) => {
-    if (ctx.query.shortid) {
-      return strapi.services.filesystem.findByShort(ctx.query.shortid)
-    } else {
-      ctx.response.badRequest("Short ID not Found")
+    get: async (ctx) => {
+        if (ctx.query.shortid) {
+            let fs = await strapi.services.filesystem.getFs({
+                shortid: ctx.query.shortid
+            }, {
+                type: 'file',
+                permission: 1,
+                userid: (ctx.state.user ? ctx.state.user._id : undefined)
+            });
+            if (fs === undefined) {
+                return ctx.notFound();
+            }
+            if (fs === null) {
+                return ctx.forbidden();
+            }
+            return strapi.services.filesystem.extractFSData(fs, false, true);
+        } else {
+            ctx.response.badRequest("Short ID not Found");
+        }
+    },
+
+    /**
+    * Retrieve a file / directory.
+    *
+    * @return {Object}
+    */
+
+    findOne: async (ctx) => {
+        if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
+            return ctx.notFound();
+        }
+
+        return strapi.services.filesystem.fetch(ctx.params);
+    },
+
+    /**
+    * Create a file / directory.
+    *
+    * @return {Object}
+    */
+
+    create: async (ctx) => {
+        return strapi.services.filesystem.add(ctx.request.params._id, ctx.request.body);
+    },
+
+    /**
+    * Create a temporatory file.
+    *
+    * @return {Object}
+    */
+
+    createTmp: async (ctx) => {
+        return strapi.services.filesystem.add(ctx.request.body);
+    },
+
+    /**
+    * Edit a file.
+    *
+    * @return {Object}
+    */
+
+    edit: async (ctx, next) => {
+        return strapi.services.filesystem.edit(ctx.params, ctx.request.body) ;
+    },
+
+    /**
+    * Move a file / directory.
+    *
+    * @return {Object}
+    */
+
+    move: async (ctx, next) => {
+        return strapi.services.filesystem.move(ctx.request.params._id, ctx.request.params.tgfs);
+    },
+
+    /**
+    * Delete a file / directory.
+    *
+    * @return {Object}
+    */
+
+    delete: async (ctx, next) => {
+        return strapi.services.filesystem.remove(ctx.params);
     }
-  },
-
-  /**
-   * Retrieve a filesystem record.
-   *
-   * @return {Object}
-   */
-
-  findOne: async (ctx) => {
-    if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
-      return ctx.notFound();
-    }
-
-    return strapi.services.filesystem.fetch(ctx.params);
-  },
-
-  /**
-   * Create a/an filesystem record.
-   *
-   * @return {Object}
-   */
-
-  create: async (ctx) => {
-    return strapi.services.filesystem.add(ctx.request.params._id, ctx.request.body);
-  },
-
-  /**
-   * Update a/an filesystem record.
-   *
-   * @return {Object}
-   */
-
-  update: async (ctx, next) => {
-    return strapi.services.filesystem.edit(ctx.params, ctx.request.body) ;
-  },
-
-  /**
-   * Destroy a/an filesystem record.
-   *
-   * @return {Object}
-   */
-
-  destroy: async (ctx, next) => {
-    return strapi.services.filesystem.remove(ctx.params);
-  }
 };
--- api/filesystem/services/Filesystem.js
+++ api/filesystem/services/Filesystem.js
@@ -1,198 +1,300 @@
 'use strict';
 
 /**
- * Filesystem.js service
- *
- * @description: A set of functions similar to controller's actions to avoid code duplication.
- */
+* Filesystem.js service
+*
+* @description: A set of functions similar to controller's actions to avoid code duplication.
+*/
 
 // Public dependencies.
 const _ = require('lodash');
+const util = require('util');
 
 module.exports = {
+    extractFSData: async function (fs, complete, extend) {
+        let ret = {
+            id: fs._id,
+            name: fs.name,
+            description: fs.description,
+            parent: fs.parent || "",
+            owner: fs.owner,
+            createDate: fs.createDate,
+            modifyDate: fs.modifyDate,
+            isPublic: fs.isPublic,
+            shortid: (fs.shortid ? fs.shortid : ''),
+            format: fs.isFile === true ? fs.format : 'Directory'
+        };
+        if (fs.isFile === true) {
+            ret.size = (new util.TextEncoder('utf-8').encode(fs.code.length).length);
+            ret.size += (new util.TextEncoder('utf-8').encode(fs.stdin.length).length);
+            ret.size += (new util.TextEncoder('utf-8').encode(fs.description.length).length);
+        }
+        if (extend == true) {
+            if (fs.parent) {
+                await fs.populate('parent').execPopulate();
+                ret.parent = await strapi.services.filesystem.extractFSData(fs.parent, false);
+            } else {
+                ret.parent = {};
+            }
+            if (fs.owner) {
+                await fs.populate('owner').execPopulate();
+                ret.owner = await strapi.services.filesystem.extractUserData(fs.owner, false);
+            }
+        }
+        if (complete == true) {
+            ret.editSecret = fs.editSecret;
+            if (fs.isFile === true) {
+                ret.code = fs.code;
+                ret.stdin = fs.stdin;
+            } else {
+                ret.files = [];
+                let dir = await fs.populate('files').execPopulate();
+                for (let chld of dir.files) {
+                    ret.files.push(await strapi.services.filesystem.extractFSData(chld, false));
+                }
+            }
+        }
+        return ret;
+    },
+    extractUserData: (user, privData) => {
+        let ret = {};
+        if (!user) {
+            return undefined;
+        } else {
+            if (privData === null) {
+                return user;
+            }
+            ret.id = user._id;
+            ret.name = user.name;
+            ret.level = user.level;
+            ret.createDate = user.createDate;
+            if (privData == true) {
+                ret.lastLogin = user.lastLogin;
+                ret.email = user.email;
+                ret.rootfsid = user.root;
+                strapi.log.debug(user.settings);
+                ret.settings = user.settings;
+            }
+        }
+        return ret;
+    },
+    /************************
+    ***    getAccess     ***
+    ************************
+    2 = Read/Write
+    1 = Read Only
+    0 = No Access
+    */
+    getAccess: (fs, userid) => {
+        if (!fs) {
+            return undefined;
+        }
+        if (strapi.services.filesystem.isTempFile(fs) == true) {
+            if (userid && userid == fs.editSecret) {
+                return 2;
+            } else {
+                return 1;
+            }
+        } else {
+            if (userid && userid.equals(fs.owner)) {
+                return 2;
+            } else if (fs.isPublic === true) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    },
 
-  /**
-   * Promise to fetch all filesystems.
-   *
-   * @return {Promise}
-   */
+    /**
+    * Promise to get a filesystem object.
+    *
+    * @return {Promise}
+    */
 
-  fetchAll: (params) => {
-    // Convert `params` object to filters compatible with Mongo.
-    const filters = strapi.utils.models.convertParams('filesystem', params);
-    // Select field to populate.
-    const populate = Filesystem.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias)
-      .join(' ');
+    getFs: async (query, condition) => {
+        let fs = await Filesystem.findOne(query);
+        if (!fs) {
+            return undefined;
+        }
+        /// Check type
+        if (condition && condition.type) {
+            if (condition.type == 'root') {
+                if (!strapi.services.filesystem.isRootDir(fs)) {
+                    return null;
+                }
+            } else if (condition.type == 'directory') {
+                if (fs.isFile != false) {
+                    return null;
+                }
+            } else if (condition.type == 'file') {
+                if (fs.isFile != true) {
+                    return null;
+                }
+            } else if (condition.type == 'tmp') {
+                if (!strapi.services.filesystem.isTempFile(fs)) {
+                    return null;
+                }
+            }
+        }
+        /// Check Permission
+        if (condition && condition.permission) {
+            if (strapi.services.filesystem.getAccess(fs, condition.userid) < condition.permission) {
+                return null;
+            }
+        }
+        return fs;
+    },
 
-    return Filesystem
-      .find()
-      .where(filters.where)
-      .sort(filters.sort)
-      .skip(filters.start)
-      .limit(filters.limit)
-      .populate(populate);
-  },
+    /**
+    * Promise to fetch a/an filesystem.
+    *
+    * @return {Promise}
+    */
 
-  /**
-   * Promise to fetch a/an filesystem.
-   *
-   * @return {Promise}
-   */
+    fetch: (params) => {
+        // Select field to populate.
+        const populate = Filesystem.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias)
+        .join(' ');
 
-  fetch: (params) => {
-    // Select field to populate.
-    const populate = Filesystem.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias)
-      .join(' ');
+        return Filesystem
+        .findOne(_.pick(params, _.keys(Filesystem.schema.paths)))
+        .populate(populate);
+    },
 
-    return Filesystem
-      .findOne(_.pick(params, _.keys(Filesystem.schema.paths)))
-      .populate(populate);
-  },
+    /**
+    * Promise to add a/an filesystem.
+    *
+    * @return {Promise}
+    */
 
-  /**
-   * Promise to count filesystems.
-   *
-   * @return {Promise}
-   */
+    add: async (values) => {
+        // Extract values related to relational data.
+        const relations = _.pick(values, Filesystem.associations.map(ast => ast.alias));
+        const data = _.omit(values, Filesystem.associations.map(ast => ast.alias));
 
-  count: (params) => {
-    // Convert `params` object to filters compatible with Mongo.
-    const filters = strapi.utils.models.convertParams('filesystem', params);
+        // Create entry with no-relational data.
+        const entry = await Filesystem.create(data);
 
-    return Filesystem
-      .count()
-      .where(filters.where);
-  },
+        // Create relational data and return the entry.
+        return Filesystem.updateRelations({ _id: entry.id, values: relations });
+    },
 
-  /**
-   * Promise to add a/an filesystem.
-   *
-   * @return {Promise}
-   */
+    /**
+    * Promise to edit a/an filesystem.
+    *
+    * @return {Promise}
+    */
 
-  add: async (values) => {
-    // Extract values related to relational data.
-    const relations = _.pick(values, Filesystem.associations.map(ast => ast.alias));
-    const data = _.omit(values, Filesystem.associations.map(ast => ast.alias));
+    edit: async (params, values) => {
+        // Extract values related to relational data.
+        const relations = _.pick(values, Filesystem.associations.map(a => a.alias));
+        const data = _.omit(values, Filesystem.associations.map(a => a.alias));
 
-    // Create entry with no-relational data.
-    const entry = await Filesystem.create(data);
+        // Update entry with no-relational data.
+        const entry = await Filesystem.update(params, data, { multi: true });
 
-    // Create relational data and return the entry.
-    return Filesystem.updateRelations({ _id: entry.id, values: relations });
-  },
+        // Update relational data and return the entry.
+        return Filesystem.updateRelations(Object.assign(params, { values: relations }));
+    },
 
-  /**
-   * Promise to edit a/an filesystem.
-   *
-   * @return {Promise}
-   */
+    /**
+    * Promise to remove a/an filesystem.
+    *
+    * @return {Promise}
+    */
 
-  edit: async (params, values) => {
-    // Extract values related to relational data.
-    const relations = _.pick(values, Filesystem.associations.map(a => a.alias));
-    const data = _.omit(values, Filesystem.associations.map(a => a.alias));
+    remove: async params => {
+        // Select field to populate.
+        const populate = Filesystem.associations
+        .filter(ast => ast.autoPopulate !== false)
+        .map(ast => ast.alias)
+        .join(' ');
 
-    // Update entry with no-relational data.
-    const entry = await Filesystem.update(params, data, { multi: true });
+        // Note: To get the full response of Mongo, use the `remove()` method
+        // or add spent the parameter `{ passRawResult: true }` as second argument.
+        const data = await Filesystem
+        .findOneAndRemove(params, {})
+        .populate(populate);
 
-    // Update relational data and return the entry.
-    return Filesystem.updateRelations(Object.assign(params, { values: relations }));
-  },
-
-  /**
-   * Promise to remove a/an filesystem.
-   *
-   * @return {Promise}
-   */
-
-  remove: async params => {
-    // Select field to populate.
-    const populate = Filesystem.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias)
-      .join(' ');
-
-    // Note: To get the full response of Mongo, use the `remove()` method
-    // or add spent the parameter `{ passRawResult: true }` as second argument.
-    const data = await Filesystem
-      .findOneAndRemove(params, {})
-      .populate(populate);
-
-    if (!data) {
-      return data;
-    }
-
-    await Promise.all(
-      Filesystem.associations.map(async association => {
-        if (!association.via || !data._id) {
-          return true;
+        if (!data) {
+            return data;
         }
 
-        const search = _.endsWith(association.nature, 'One') || association.nature === 'oneToMany' ? { [association.via]: data._id } : { [association.via]: { $in: [data._id] } };
-        const update = _.endsWith(association.nature, 'One') || association.nature === 'oneToMany' ? { [association.via]: null } : { $pull: { [association.via]: data._id } };
+        await Promise.all(
+            Filesystem.associations.map(async association => {
+                if (!association.via || !data._id) {
+                    return true;
+                }
 
-        // Retrieve model.
-        const model = association.plugin ?
-          strapi.plugins[association.plugin].models[association.model || association.collection] :
-          strapi.models[association.model || association.collection];
+                const search = _.endsWith(association.nature, 'One') || association.nature === 'oneToMany' ? { [association.via]: data._id } : { [association.via]: { $in: [data._id] } };
+                const update = _.endsWith(association.nature, 'One') || association.nature === 'oneToMany' ? { [association.via]: null } : { $pull: { [association.via]: data._id } };
 
-        return model.update(search, update, { multi: true });
-      })
-    );
+                // Retrieve model.
+                const model = association.plugin ?
+                strapi.plugins[association.plugin].models[association.model || association.collection] :
+                strapi.models[association.model || association.collection];
 
-    return data;
-  },
+                return model.update(search, update, { multi: true });
+            })
+        );
 
-  /**
-   * Promise to search a/an filesystem.
-   *
-   * @return {Promise}
-   */
+        return data;
+    },
 
-  search: async (params) => {
-    // Convert `params` object to filters compatible with Mongo.
-    const filters = strapi.utils.models.convertParams('filesystem', params);
-    // Select field to populate.
-    const populate = Filesystem.associations
-      .filter(ast => ast.autoPopulate !== false)
-      .map(ast => ast.alias)
-      .join(' ');
+        /**
+        * Promise to search a/an filesystem.
+        *
+        * @return {Promise}
+        */
 
-    const $or = Object.keys(Filesystem.attributes).reduce((acc, curr) => {
-      switch (Filesystem.attributes[curr].type) {
-        case 'integer':
-        case 'float':
-        case 'decimal':
-          if (!_.isNaN(_.toNumber(params._q))) {
-            return acc.concat({ [curr]: params._q });
-          }
+        search: async (params) => {
+            // Convert `params` object to filters compatible with Mongo.
+            const filters = strapi.utils.models.convertParams('filesystem', params);
+            // Select field to populate.
+            const populate = Filesystem.associations
+            .filter(ast => ast.autoPopulate !== false)
+            .map(ast => ast.alias)
+            .join(' ');
 
-          return acc;
-        case 'string':
-        case 'text':
-        case 'password':
-          return acc.concat({ [curr]: { $regex: params._q, $options: 'i' } });
-        case 'boolean':
-          if (params._q === 'true' || params._q === 'false') {
-            return acc.concat({ [curr]: params._q === 'true' });
-          }
+            const $or = Object.keys(Filesystem.attributes).reduce((acc, curr) => {
+                switch (Filesystem.attributes[curr].type) {
+                    case 'integer':
+                    case 'float':
+                    case 'decimal':
+                    if (!_.isNaN(_.toNumber(params._q))) {
+                        return acc.concat({ [curr]: params._q });
+                    }
 
-          return acc;
-        default:
-          return acc;
-      }
-    }, []);
+                    return acc;
+                    case 'string':
+                    case 'text':
+                    case 'password':
+                    return acc.concat({ [curr]: { $regex: params._q, $options: 'i' } });
+                    case 'boolean':
+                    if (params._q === 'true' || params._q === 'false') {
+                        return acc.concat({ [curr]: params._q === 'true' });
+                    }
 
-    return Filesystem
-      .find({ $or })
-      .sort(filters.sort)
-      .skip(filters.start)
-      .limit(filters.limit)
-      .populate(populate);
-  }
+                    return acc;
+                    default:
+                    return acc;
+                }
+            }, []);
+
+            return Filesystem
+            .find({ $or })
+            .sort(filters.sort)
+            .skip(filters.start)
+            .limit(filters.limit)
+            .populate(populate);
+    },
+    isRootDir: (fs) => {
+        return (!fs.parent && fs.name == 'Root' && fs.isFile == false)
+    },
+    isTempFile: (fs) => {
+        return (!fs.parent && !fs.owner && fs.isFile == true)
+    }
 };
